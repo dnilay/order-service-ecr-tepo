c67a70bb16ccd4d39b032221907ed7c047c7b331
Nilay <mailnilaydas@gmail.com>
2023-03-30 16:42:13 +0530

    8th commit

--- src/main/java/com/javatechie/aws/cicd/example/OrderServiceApplication.java
+++ src/main/java/com/javatechie/aws/cicd/example/OrderServiceApplication.java
@@ -1,12 +1,10 @@
 package com.javatechie.aws.cicd.example;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
